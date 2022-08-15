645ff3425f08246637aaf4de1eb50adf9db95ab2
AmazonnStudio <amazonstudio69@gmail.com>
2022-08-15 22:45:28 +0600

    send

---
+++ Intro-variable.js
@@ -0,0 +1,13 @@
+// Decliar Variable :
+    var info = 'Freelancer Nasim' ;
+
+// Variable Value Type :
+    var onionPrice = 12 ;
+
+    var firstBoy = 'Freelacer Afridi';
+    var lastBoy = "Shohagh" ; 
+
+    var youIndian = False;
+    var muslim = true;
+
+// Variable Name Decliar : 
--- variable-name.js
+++
@@ -1,3 +0,0 @@
-// perfect way to decliar Name
-var myHomeAddress = 'South sorma ';
-var my_home_
